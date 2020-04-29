732599e6ae4116378a2d51007e84e783db5de5a7
laurenyz <laurenkyu@gmail.com>
2020-04-29 13:02:51 -0400

    updated scratchpad component not using redux

--- src/components/ScratchPad.jsx
+++ src/components/ScratchPad.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import '../ScratchPad.css'
 
-const ScratchPad = () => {
-    return(<div>
-        ScratchPad
-    </div>)
+class ScratchPad extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            text: ""
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/scratchpads/1')
+        .then(resp => resp.json())
+        .then(scratchpad => this.setState({text: scratchpad.scratchpad_text}))
+    }
+
+    handleOnChange = event => {
+        this.setState({text: event.target.value})
+    }
+    
+    render() {
+          return (
+            <textarea
+              value={this.state.text}
+              onChange={this.handleOnChange}
+            />
+          )
+        }
+
 }
 
 export default ScratchPad
+
+// import React, { Component } from 'react'
+// import '../ScratchPad.css'
+
+// class ScratchPad extends Component {
+//   state = { text: '' }
+
+//   componentDidMount() {
+//     fetch('http://localhost:3000/scratchpads/1')
+//     .then(data => {data.json()
+//     .then(res => {
+//           console.log(res)
+//         this.setState({ text: res.scratchpad_text })
+//       })
+//     })
+//   }
+  
+//   handleChange = e => {
+//     this.setState({ text: e.target.value })
+//   }
+
+//   render() {
+//     return (
+//       <textarea
+//         value={this.state.text}
+//         onChange={this.handleChange}
+//       />
+//     )
+//   }
+// }
+
+// export default ScratchPad
