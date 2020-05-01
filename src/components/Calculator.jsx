57ac33fb5bf301ffc543608113bebcd6fa7154d2
laurenyz <laurenkyu@gmail.com>
2020-04-30 22:11:42 -0400

    removed desmos calulcator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import Desmos from 'desmos'
 
 class Calculator extends React.Component {
 
-    // displayCalculator = (elt) => {
-    //     const calculator = Desmos.GraphingCalculator(elt)
-    //     calculator.setExpression({ id: 'graph1', latex: 'y=x^2' })
-
-    // }
 
     render(){
         console.dir(Desmos.GraphingCalculator(<div style = {{width: "600px", height: "400px"}}></div>))
         return(
             <div>
             Calculator
-            
-            {/* {this.displayCalculator(<div style = {{width: "600px", height: "400px"}}></div>)} */}
-            
             </div>)
     }
 }
