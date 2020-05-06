74e92b3203c3cb5a61c49a5a67d72bbb03094787
laurenyz <laurenkyu@gmail.com>
2020-05-06 13:21:35 -0400

    edit name directly in editer

--- src/containers/ScratchPadContainer.jsx
+++ src/containers/ScratchPadContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import ScratchPad from '../components/ScratchPad'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
@@ -9,6 +9,9 @@ import Dialog from '@material-ui/core/Dialog';
 import NewScratchPadForm from '../components/NewScratchPadForm'
 import ReactToPrint from "react-to-print";
 import PrintIcon from '@material-ui/icons/Print';
+import TextField from '@material-ui/core/TextField';
+import { editingSavedScratchpad } from '../redux/actions'
+import Grid from '@material-ui/core/Grid'
 
 
 
@@ -21,11 +24,21 @@ const useStyles = makeStyles((theme) => ({
 const ScratchPadContainer = (props) => {
   const componentRef = useRef();
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [name, setName] = useState("")
+    const [scratchpad, setScratchpad] = useState(null)
+
+    useEffect(() => {
+      if(props.userScratchpads.find(pad => pad.url===props.match.params.url)){
+        const pad = props.userScratchpads.find(pad => pad.url===props.match.params.url)
+        setScratchpad(pad)
+        setName(pad.name)
+      } 
+    }, [props.userScratchpads, props.match.params.url])
     
     const handleClickOpen = () => {
         setOpen(true);
-    };
+    }
     
     const handleClose = () => {
         setOpen(false);
@@ -34,39 +47,44 @@ const ScratchPadContainer = (props) => {
       <div>
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
               <NewScratchPadForm url={props.match.params.url} handleClose={handleClose} />
-          </Dialog>
-        {props.userScratchpads.find(pad => pad.url===props.match.params.url)?
-         <Button
-         variant="contained"
-         disabled={true}
-         color="primary"
-         size="small"
-         className={classes.button}
-         startIcon={<SaveIcon />}
-         onClick = {handleClickOpen}
-         >
-           Save
-         </Button>    
-        :
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          className={classes.button}
-          startIcon={<SaveIcon />}
-          onClick = {handleSaveOnClick}
-          >
-            Save
-          </Button> 
-      }
-     <ReactToPrint
-        trigger={() => <Button variant="contained" color="primary" size="small" startIcon={<PrintIcon />}>Print</Button>}
-        content={() => componentRef.current}
-      />
-      <div ref={componentRef}>
-          <ScratchPad />
-          </div>
+        </Dialog>
+        <Grid container>
+            <Grid item xs={3} />
+            <Grid item xs={6}>
+              <Grid container justify="center">
+              <Grid container justify="center" alignItems="center" style={{marginTop: "20px"}}>
+                <Grid item>
+                {scratchpad?
+                  <Button variant="contained" disabled={true} color="primary" size="small" className={classes.button} startIcon={<SaveIcon />} onClick = {handleClickOpen}> Save </Button>    
+                  :<Button variant="contained" color="primary" size="small" className={classes.button} startIcon={<SaveIcon />} onClick = {handleSaveOnClick}>Save</Button> 
+                }
+                </Grid>
+                <Grid item>
+                  <ReactToPrint
+                    trigger={() => <Button variant="contained" color="primary" size="small" startIcon={<PrintIcon />}>Print</Button>}
+                    content={() => componentRef.current}
+                  />
+                </Grid>
+                <Grid item style={{marginLeft: "10px"}}>
+                {scratchpad?
+                    <TextField autoFocus variant="outlined" margin="dense" name="name" label="Document Title" type="name" onChange = {handleOnChange} value = {name}/>:null}
+                </Grid>
+              </Grid>
+              <Grid item></Grid>
+                <div ref={componentRef}>
+                    <ScratchPad />
+                </div>
+              </Grid>
+            </Grid>
+            <Grid item xs={3} />
+        </Grid>
+          
       </div>)   
+
+    function handleOnChange(event){
+        // setName(event.target.value)
+        props.editingSavedScratchpad({id: scratchpad.id, name: event.target.value})
+    }
 
    function handleSaveOnClick(){
       if (props.user){
@@ -85,4 +103,10 @@ const mapStateToProps = state => {
     }
 }
 
-export default withRouter((connect(mapStateToProps))(ScratchPadContainer))
+const mapDispatchToProps = dispatch => {
+  return {
+      editingSavedScratchpad: userInfo => dispatch(editingSavedScratchpad(userInfo))
+  }
+}
+
+export default withRouter((connect(mapStateToProps, mapDispatchToProps))(ScratchPadContainer))
